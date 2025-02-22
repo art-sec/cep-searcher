1a753f9cc781dc4af4af25ee7bf4a5afcb9ee731
art-sec <arthurfoiceiro@gmail.com>
2025-02-22 19:16:40 -0300

    some changes!

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
 from selenium import webdriver
 import time
 
-# Create the variables that contains the website and the CEP
+# Create the variables that contains the website and the ZIP code
 WEBSITE = "https://www.siterastreio.com.br/cep"
-CEP = "02762-060"
+ZIP_CODE = "02762-060"
 
 # Initiallize the webdriver and the browser
 browser = webdriver.Edge()
 browser.get(WEBSITE)
 time.sleep(3)
 
-# Input the CEP value on the input box
+# Input the ZIP code value on the input box
 inputCep = browser.find_element('xpath', '/html/body/section[1]/div/div/div[1]/div/input')
-inputCep.send_keys(CEP)
+inputCep.send_keys(ZIP_CODE)
 time.sleep(2)
 
-# Confirm the CEP search on the website and return the cep informations on the user's screen
+# Confirm the ZIP code search on the website and return the ZIP code informations on the user's screen
 confirmSearch = browser.find_element('xpath', '/html/body/section[1]/div/div/div[1]/div/a/button')
 confirmSearch.click()
 time.sleep(5)
